6aaf58acc04ce3897ae600f9727b575cd5875409
jasonligg <>
2020-11-20 12:51:17 -0500

    basic render Nav bar with React Router and Hooks

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,22 @@
 /* eslint-disable react/prefer-stateless-function */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
 
-class App extends React.Component {
-  render() {
-    return <h1>Hello World!</h1>;
-  }
-}
+const App = () => {
+  // return <h1>Hello Jordan!</h1>;
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          {/* <Route path="/" exact component={Home} /> */}
+          {/* <Route path="/closet" component={Closet} /> */}
+          {/* <Route path="/donation" component={Donation} /> */}
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
