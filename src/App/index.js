18b73077c2f473f96e0fa7b08eccd95cf771aae0
jasonligg <>
2020-11-20 10:43:25 -0500

    boilerplate code

---
+++ src/App/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import style from './static/style.css';
+// eslint-disable-next-line import/extensions
+import App from './App.jsx';
+
+ReactDOM.render(<App name="I am app!" />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const config = {
+  entry: './src/App/index.js',
+  output: {
+    path: path.resolve(`${__dirname}/build`),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  devServer: {
+    proxy: {
+      // '/api': 'http://localhost:3000/',
+      // '/signup': 'http://localhost:3000/',
+      // '/login': 'http://localhost:3000/',
+      // '/authorized': 'http://localhost:3000/',
+    },
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+
+module.exports = config;
