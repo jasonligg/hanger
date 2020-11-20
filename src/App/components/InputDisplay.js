cf8f07fd69b7d06d94145fc01d3d270dc5c86570
jasonligg <>
2020-11-20 17:41:36 -0500

    InputDisplay component has state that is almost ready to be sent to backend. Need to implement file reading and actual fetch request functionality.

--- src/App/components/InputDisplay.js
+++ src/App/components/InputDisplay.js
@@ -1,37 +1,55 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
+// when updating state, we are putting into the userInput state object
+// key-value pairs. the four pairs are:
+// itemName: String -> user types into texbar
+// itemClothingType: String  -> user selects from dropdown bar
+// itemColor: String -> user selects from dropdown bar
+// itemImage: File API integration -> user selects from file from device storage
 const InputDisplay = () => {
   const initialState = {
     itemName: '',
-    itemType: '',
-    itemColor: '',
+    itemClothingType: 'Tops/Shirts/Tees',
+    itemColor: 'Black',
     itemImage: null,
   };
   const [userInput, setUserInput] = useState(initialState);
 
-  // when updating state, we are putting into the userInput state object
-  // key-value pairs. the four pairs are:
-  // itemName: String -> user types into texbar
-  // itemClothingType: String  -> user selects from dropdown bar
-  // itemColor: String -> user selects from dropdown bar
-  // itemImage: File API integration -> user selects from file from device storage
+  const handleImageUpload = (event) => {
+    const [file] = event.target.files;
+    if (file) {
+      console.log(file);
+    }
+  }
 
   return (
     <div className="InputDisplay">
       <p>Add to Closet</p>
       <p>Upload Image</p>
-      <input id="upload-file" type="file" />
+      <input
+        id="upload-file"
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        multiple="false"
+      />
       <input
         type="text"
         onChange={(event) =>
-          setUserInput({ ...initialState, itemName: event.target.value })
+          setUserInput({ ...userInput, itemName: event.target.value })
         }
       />
       <button type="button">Submit</button>
       <div className="dropDowns">
         <h3>Select Color</h3>
-        <select className="color">
+        <select
+          className="color"
+          value={userInput.itemColor}
+          onChange={(event) => {
+            setUserInput({ ...userInput, itemColor: event.target.value });
+          }}
+        >
           <option value="Black">Black</option>
           <option value="White">White</option>
           <option value="Red">Red</option>
@@ -45,7 +63,13 @@ const InputDisplay = () => {
           <option value="Grey">Grey</option>
         </select>
         <h3>Select Type</h3>
-        <select className="clothingType">
+        <select
+          className="clothingType"
+          value={userInput.itemClothingType}
+          onChange={(event) =>
+            setUserInput({ ...userInput, itemClothingType: event.target.value })
+          }
+        >
           <option value="Tops/shirts/Tees">Tops/Shirts/Tees</option>
           <option value="Pants">Pants</option>
           <option value="Skirts">Skirt</option>
