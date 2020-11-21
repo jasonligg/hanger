0ceb7782bde1d43366c8df294da562bfed31798f
Jordan Grubb <imjordangrubb@gmail.com>
2020-11-21 13:24:46 -0500

    Landing page skeleton complete

--- src/App/components/Landing.js
+++ src/App/components/Landing.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  NavLink,
+} from 'react-router-dom';
 
 const Landing = () => {
   return (
     <div className="main">
       <img id="logo" src="./src/Images/HangerLogo.png" alt="hangerLogo" />
+      <button>
+        <NavLink to="/closet">View Closet</NavLink>
+      </button>
     </div>
   );
 };
