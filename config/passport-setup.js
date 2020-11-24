bef48420d2c9ab38c7a3ed04eda3854e6f4e793e
Sabrina <s.goldfarb2@gmail.com>
2020-11-24 09:48:02 -0600

    added files to gitignore. working through passportjs

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -12,7 +12,7 @@ passport.use(
     //need a client ID and a client secret
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
-}, (accessToken, refreshToken,profile,done) => {
+}, async (accessToken, refreshToken,profile,done) => {
     //passport callback function
     console.log('passport callback function fired')
     // console.log(profile.name.givenName,profile.name.familyName,profile.emails[0].value, profile.photos[0].value)
@@ -20,8 +20,12 @@ passport.use(
     const emails = profile.emails[0].value
     const profileImage = profile.photos[0].value
     console.log(givenName, familyName, emails, profileImage)
-    const queryStr = 'INSERT INTO Users (first_name1, last_name1, email, profile_Image) VALUES ($1, $2, $3, $4)';
-    const queryParams = [givenName, familyName, emails, profileImage];
-    db.query(queryStr,queryParams)
+    const queryStr = 'INSERT INTO Users (first_name1, last_name1, username, email, profile_Image, display_name_1) VALUES ($1, $2, $3, $4, $5, $6)';
+    const queryParams = [givenName, familyName, emails, emails, profileImage, givenName];
+    try {
+    await db.query(queryStr,queryParams)
+    } catch (error) {
+        return 'There\'s an error, fool'
+    }
 })
 )
