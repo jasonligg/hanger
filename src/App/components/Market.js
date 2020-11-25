ec621df2e5a0e167c221bf07c35bef34e791f949
mggower <gowermikey@gmail.com>
2020-11-25 15:33:14 -0500

    merge jordan

--- src/App/components/Market.js
+++ src/App/components/Market.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import ItemCard from './ItemCard';
+import MarketCard from './MarketCard';
 
-const Market = () => {
-  const demo = [];
-  for (let i = 0; i < 15; i += 1) {
-    demo.push(<ItemCard />);
-  }
-
-  return <div className="closet">{demo}</div>;
+const Market = ({ marketData }) => {
+  const cards = marketData.map((item, i) => (
+    <MarketCard key={`mkItem${i}`} id={item._id} item={item} />
+  ));
+  return <div className="closet">{cards}</div>;
 };
-
+//loop through items that are available and push them into item card with item information
+//reference closet
 export default Market;
--- src/App/containers/MarketplaceContainer.js
+++ src/App/containers/MarketplaceContainer.js
@@ -1,22 +1,35 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import Market from '../components/Market';
 
 const MarketplaceContainer = () => {
+  const [market, setMarkets] = useState();
+  const [hasLoaded, setHasLoaded] = useState(false);
 
+  /*upon component mount, make fetch request to get all clothes that don't belong to user 
+  which are marked as available*/
   useEffect(() => {
-    effect
-    return () => {
-      cleanup
-    }
-  }, [input])
-  // '/api/marketplace
+    fetch('/api/marketplace')
+      .then((response) => response.json())
+      .then((data) => {
+        setMarkets(data);
+        setHasLoaded(true);
+      })
+      //catch all other errors
+      .catch((error) => console.log(error));
+  }, []);
 
-  return (
+  return hasLoaded ? (
+    <div>
+      <div className="content-nav"></div>
+      <div className="content-container">
+        <h1>Marketplace</h1>
+        <Market marketData={market} />
+      </div>
+    </div>
+  ) : (
     <div className="content-container">
-      <h1>Marketplace Container</h1>
-      <Market />
+      <p>Still loading...</p>
     </div>
   );
 };
-
 export default MarketplaceContainer;
