efce50299dc91d13a121cf658ab0ec96cfac4806
Brooke Luro <brookeluro@Brookes-MacBook-Pro.local>
2020-11-25 15:42:43 -0500

    added oauth page code

--- routes/oauth-routes.js
+++ routes/oauth-routes.js
@@ -8,8 +8,31 @@ const passport = require('passport');
 //auth login
 router.get('/login', (req, res) => {
   //here is where we serve up our login page
-  res.send('put something here later, he had res.render his login page');
+  res.send('');
 });
+
+//auth with google
+router.get(
+  '/facebook',
+  passport.authenticate('facebook', {
+    scope: ['email', 'public_profile'],
+  })
+);
+
+router.get(
+  '/facebook/callback',
+  passport.authenticate('facebook'),
+  (req, res) => {
+    // successRedirect: '/',
+    // failureRedirect: '/login',
+    // res.locals.isVerified = true;
+    const user = req.user.rows[0]._id;
+    // res.redirect(`/api/${user}`);
+    // res.json(user);
+    res.cookie('success', user);
+    res.redirect('/');
+  }
+);
 
 //auth with google
 router.get(
@@ -23,6 +46,7 @@ router.get(
 router.get('/logout', (req, res) => {
   //this will be handled later with passport
   req.logout();
+
   res.redirect('/');
 });
 
@@ -39,8 +63,22 @@ router.get('/logout', (req, res) => {
 // }
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
   // res.send(req.user)
+  console.log(req.user.rows[0]._id);
   //sending our OAuth user data
+  res.locals.isVerified = true;
+  const user = req.user.rows[0]._id;
+  // res.redirect(`/api/${user}`);
+  // res.json(user);
+  res.cookie('success', user);
   res.redirect('/');
+
+  //router needs to route to where we want to render
+  //if the route is to a username as the endpoint, write a function that renders an html page with everything for their closet
+  //make another get request to get the user information that is stored in the database
+  //as far as react router -- the only private route we need is instead of sending it to /closet, send it to /closet/joeschmoe and /closet would need some js that would populate /:name, which can be done as a route in middleware
+  //render the page with the data you get back from /:name
+  //with that :name absolute path, you can send that in react router, but in the frontend you would have to create that path
+  //you could create it if you have a function that runs when you have a path to go to
 });
 
 module.exports = router;
